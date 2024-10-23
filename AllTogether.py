427ad84626e704130b471bbdf26e41dbf499d1e7
yusei-Soma <ys338@kent.ac.uk>
2024-10-24 02:22:45 +0900

    Update AllTogether.py

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,12 @@
 ### Part Four -- your code goes here. 
+import random
+x = [random.randint(1, 100) for _ in range(10)]
+print("first List:", x)
+y = 0
+while y < len(x):
+    if x[y] % 2 == 0:
+        x.pop(y)
+    else:
+        y += 1
+
+print("Odd Numbers:",x)
